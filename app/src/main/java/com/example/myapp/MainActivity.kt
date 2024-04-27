3afb5f8416e8ed02878fe3ca4bdf46252c3e95fb
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-04-27 20:40:43 +0545

    changes

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,17 +4,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(){
+    lateinit var press: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        press= findViewById(R.id.button2)
+
+        press.setOnClickListener {
+            Toast.makeText(applicationContext,"You are signed up",
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+
     }
 }
